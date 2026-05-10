d8b68dd5be0db52e56c4cd02e7f4cbda51713315
GauravMahale12 <gauravmahale0111@gmail.com>
2026-05-10 20:14:13 +0530

    Testing startup

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,5 @@
-from database import engine
-from models import Base
-
-Base.metadata.create_all(bind=engine)
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from routes import router
 
 app = FastAPI(
     title="HealthAI Pro API",
@@ -17,18 +10,14 @@ app = FastAPI(
 # CORS Setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # change in production
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Root Test Route
 @app.get("/")
 def home():
     return {
         "message": "HealthAI Pro Backend Running Successfully"
     }
-
-# All API Routes
-app.include_router(router)
